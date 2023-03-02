c81aa6c135d6331707b7e88f71e5041dbab60c11
olenarudnytskaa <elenarudnitskaya77@gmail.com>
2023-03-02 17:17:33 +0100

    first commit

--- html/lesson1.js
+++ html/lesson1.js
@@ -4,7 +4,7 @@ var discount = 0.03;
 var  total = (price - price * discount) * 10;
 console.log(total);
 
-//let oneside = prompt('enter number');
-//let secondside = prompt('enter number');
-//let fullside = oneside * secondside;
-//console.log(fullside);
+let oneside = prompt('enter number');
+let secondside = prompt('enter number');
+let fullside = oneside * secondside;
+console.log(fullside);
