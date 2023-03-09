dc3d111cf0f0f060e79a6449db8b2caef08dc37a
olenarudnytskaa <elenarudnitskaya77@gmail.com>
2023-03-09 16:41:21 +0100

    first commit

---
+++ javascript/lesson2.js
@@ -0,0 +1,43 @@
+//first task 
+//var a = prompt (`enter a`)
+//var b = prompt (`enter b`)
+
+//if (a + b < 4){
+    //result = `less`
+//} else {
+    //result = `many`
+//}
+//document.write(result);
+
+
+
+//second task 
+//var login =  prompt (`enter login`)
+
+//if (login == `Vasya`) {
+    //message = `Hi` ;
+//} else if (login == `Boss`){
+    //message = `Hello`
+//} else if (login == ``){
+    //message = `no login`
+//}
+//document.write(message);
+
+//third task 
+//let a = +prompt (`enter A`)
+//let b = +prompt (`enter B`)
+
+//for (let i = a ; i < b; i++)
+//console.log(i);
+
+//let a = +prompt (`enter A`)
+//let b = +prompt (`enter B`)
+
+//for (let i = a ; i < b; i++)
+    //if (i%2 == 0)
+//console.log(i);
+
+
+//fourth task 
+for(let counter = 0; counter < 10; counter++)
+console.log(i);
