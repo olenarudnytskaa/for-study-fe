8dc85d1d33b70136bcd17cc525bf791b0d8be3c7
olenarudnytskaa <elenarudnitskaya77@gmail.com>
2023-05-09 12:27:21 +0200

    first commit

--- javascript/lesson8.js
+++ javascript/lesson8.js
@@ -18,3 +18,97 @@
 
 
 // * <li>Попрацювати з файлом 08 */}
+
+    
+const [...div] = document.getElementsByTagName("div");
+div.forEach((element) => {
+element.style.color = "red"
+});
+
+function getuserText(){
+    const userText = document.getElementById("userText").value;
+    document.body.append(userText);
+}
+
+function cleanuserText(){
+    const userText = document.getElementById("userText").value ="";
+    document.body.append(userText);
+}
+
+
+const buttonn = document.createElement('button' );
+buttonn.type = 'text';
+document.body.append(buttonn);
+
+
+
+let image = document.querySelector('#img');
+let images = [
+    'https://itproger.com/img/courses/1476977240.jpg',
+    'https://itproger.com/img/courses/1476977488.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png'
+  ];
+  let index = 0;
+  function changePictures() {
+    index = (index + 1) % images.length; 
+    image.src = images[index]; 
+  }
+   
+const paragraph = document.querySelector("p");
+ onclick = function() {
+    document.querySelector('p').hidden = true;
+ }
+
+const  inputtext = document.querySelector ('#input1');
+const  InputText = document.querySelector ('#input2');
+function changeInput(){
+    let buffer = input1.value;
+	input1.value = input2.value;
+	input2.value= buffer;
+    } 
+    
+
+function selectFirstChild(){
+    const ul = document.querySelector('#list');
+    ul.firstElementChild.style.color = "red";
+    }
+
+function selectLastChild(){
+    const ul = document.querySelector('#list');
+    ul.lastElementChild.style.color = "red";
+    }
+
+function selectNextNode(){
+    const ul = document.querySelector('li').nextElementSibling;
+    ul.nextElementSibling.style.color = "red";
+    }
+
+function selectPrevNode(){
+    const parent = document.getElementById('list-item-3');
+    const previous = parent.previousElementSibling;
+    previous.style.color = "red";
+    }
+
+function createNewChild(){
+    const li = document.createElement('li');
+    li.innerHTML = "List Item 6"; 
+    list.append(li);
+}
+
+function removeLastChild(){
+    const ul = document.querySelector('#list');
+    ul.lastElementChild.remove(ul);
+    }
+
+function createNewChildAtStart(){
+    const li = document.createElement('li');
+    li.innerHTML = "List Item 0"; 
+    list.prepend(li);
+    }
+
+
+
+
+
+
+
